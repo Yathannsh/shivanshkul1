b72985b8454fdba106a18950a057ac0cc3c331aa
Yathannsh Kulshrestha <yathannsh@gmail.com>
2020-02-01 21:31:09 -0600

    first commit

---
+++ assets/js/cookies.js
@@ -0,0 +1,5 @@
+function CreateCookie(name,value,days){var date=new Date();date.setTime(date.getTime()+(days*24*60*60*1000));var expires="; expires="+ date.toGMTString();document.cookie=name+"="+value+expires+"; path=/";}
+function ReadCookie(name){var nameEQ=name+"=";var ca=document.cookie.split(';');for(var i=0;i<ca.length;i++){var c=ca[i];while(c.charAt(0)==' ')c=c.substring(1,c.length);if(c.indexOf(nameEQ)==0)return c.substring(nameEQ.length,c.length);}
+return null;}
+window.onload=CheckCookies;function CheckCookies(){if(ReadCookie('cookies_accepted')!='T'){var message_container=document.createElement('div');message_container.id='cookies-message-container';var html_code='<div id="cookies-message" class="alert alert-warning"><span>Toggle to Goggle </span> <a href="javascript:close();" id="accept-cookies-checkbox" name="accept-cookies" class="btn btn-default">Okay!</a></div>';message_container.innerHTML=html_code;document.body.appendChild(message_container);}}
+function close(){CreateCookie('cookies_accepted','T',365);document.getElementById('cookies-message-container').removeChild(document.getElementById('cookies-message'));}
